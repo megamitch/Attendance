ef014dd35a825ccde0d4e271167a00cbb4d9db34
joseph-campomanes <marcjoseph.albaran@gmail.com>
2015-01-08 15:05:55 +0800

    updated datatable.js

--- data/assets/mot-attendance/js/datatable.js
+++ data/assets/mot-attendance/js/datatable.js
@@ -24,31 +24,6 @@ $(document).ready( function () {
       "aaSorting":[[0, "desc"]],
       "bJQueryUI":true
       });
-      $('#datatables10').dataTable( {
-
-      "sScrollX": "200%", 
-      "bScrollCollapse": true,
-      "sScrollY": "420px",
-      "sDom": 'T<"clear">lfrtip',
-          "oTableTools": {
-              "sSwfPath": "../../../../assets/datatables/extras/TableTools/media/swf/copy_csv_xls_pdf.swf"
-      }, 
-      "aoColumns": [
-          null,
-          null,
-          null,
-          null,
-          {"sType": "currency"},
-          null,
-          null,
-          null,
-          null,
-          {"sType": "currency"}
-      ],
-      "sPaginationType":"full_numbers",
-      "aaSorting":[[0, "desc"]],
-      "bJQueryUI":true
-      });
     $('#datatables12').dataTable( {
 
       "sScrollX": "200%", 
